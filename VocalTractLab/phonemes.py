d88e283c972db281062c6c067f8a0c2bdf2eda58
PaKr1234 <paul_konstantin.krug@tu-dresden.de>
2022-01-31 21:56:31 +0100

    Added phonemes.py

--- VocalTractLab/phonemes.py
+++ VocalTractLab/phonemes.py
@@ -31,33 +31,113 @@
 #---------------------------------------------------------------------------------------------------------------------------------------------------#
 # Load essential packages:
 
+import os
 import warnings
 import numpy as np
 import pandas as pd
-from scipy.special import binom
-from scipy.special import factorial
-import matplotlib.pyplot as plt
-from itertools import zip_longest
 from itertools import chain
-from collections import Counter
-
-from VocalTractLab import plotting_tools as PT
-from VocalTractLab.plotting_tools import finalize_plot
-from VocalTractLab.plotting_tools import get_plot
-from VocalTractLab.plotting_tools import get_plot_limits
-from VocalTractLab.plotting_tools import get_valid_tiers
-from VocalTractLab import function_tools as FT
-from VocalTractLab.function_tools import is_iterable
-#from VocalTractLab import tract_sequence as TS
-from VocalTractLab.tract_sequence import Sub_Glottal_Sequence, Supra_Glottal_Sequence, Motor_Sequence
-from VocalTractLab.audio_tools import get_f0
-import VocalTractLab.VocalTractLabApi as vtl
-from VocalTractLab.target_estimation import fit
 
 
+#from VocalTractLab import plotting_tools as PT
+#from VocalTractLab.plotting_tools import finalize_plot
+#from VocalTractLab.plotting_tools import get_plot
+#from VocalTractLab.plotting_tools import get_plot_limits
+#from VocalTractLab.plotting_tools import get_valid_tiers
+#from VocalTractLab import function_tools as FT
+#from VocalTractLab.function_tools import is_iterable
+##from VocalTractLab import tract_sequence as TS
+#from VocalTractLab.tract_sequence import Sub_Glottal_Sequence, Supra_Glottal_Sequence, Motor_Sequence
+#from VocalTractLab.audio_tools import get_f0
+#import VocalTractLab.VocalTractLabApi as vtl
+#from VocalTractLab.target_estimation import fit
 
-def vowels(
-	type,
+from vtl_phonemes import phonemes as phonemes
+from VocalTractLab.function_tools import check_if_list_is_valid
+
+
+
+
+
+#---------------------------------------------------------------------------------------------------------------------------------------------------#
+def get_phoneme_list(
+	data,
+	unique_phonemes = True,
+	flatten = True,
 	):
-	phonemes = pd.read_csv()
-	return
+	if unique_phonemes:
+		return [ x[ 0 ] for x in data ]
+	else:
+		if flatten:
+			return list( chain( *data ) )
+		else:
+			return [ x for x in data ]
+#---------------------------------------------------------------------------------------------------------------------------------------------------#
+def consonants(
+	phoneme_classes = [ 'plosive', 'fricative', 'nasal', 'lateral', 'affricate' ],
+	**kwargs,
+	):
+	phoneme_classes = check_if_list_is_valid( phoneme_classes, str )
+	vowels = phonemes.loc[
+		( phonemes[ 'phoneme_type' ] == 'consonant' ) &
+		( phonemes[ 'phoneme_class' ].isin( phoneme_classes ) )
+		].sampa.to_numpy()
+	return get_phoneme_list(
+		data = vowels,
+		**kwargs,
+		)
+#---------------------------------------------------------------------------------------------------------------------------------------------------#
+def single_consonants( **kwargs ):
+	return consonants( phoneme_classes = [ 'plosive', 'fricative', 'nasal', 'lateral' ], **kwargs )
+#---------------------------------------------------------------------------------------------------------------------------------------------------#
+def plosives( **kwargs ):
+	return consonants( phoneme_classes = [ 'plosive' ], **kwargs )
+#---------------------------------------------------------------------------------------------------------------------------------------------------#
+def stops( **kwargs ):
+	return consonants( phoneme_classes = [ 'plosive', 'affricate' ], **kwargs )
+#---------------------------------------------------------------------------------------------------------------------------------------------------#
+def affricates( **kwargs ):
+	return consonants( phoneme_classes = [ 'affricate' ], **kwargs )
+#---------------------------------------------------------------------------------------------------------------------------------------------------#
+def fricatives( **kwargs ):
+	return consonants( phoneme_classes = [ 'fricative' ], **kwargs )
+#---------------------------------------------------------------------------------------------------------------------------------------------------#
+def obstruents( **kwargs ):
+	return consonants( phoneme_classes = [ 'plosive', 'fricative', 'affricate' ], **kwargs )
+#---------------------------------------------------------------------------------------------------------------------------------------------------#
+def sonorants( **kwargs ):
+	return consonants( phoneme_classes = [ 'nasal', 'lateral', 'central' ], **kwargs )
+#---------------------------------------------------------------------------------------------------------------------------------------------------#
+def approximants( **kwargs ):
+	return consonants( phoneme_classes = [ 'lateral', 'central' ], **kwargs )
+#---------------------------------------------------------------------------------------------------------------------------------------------------#
+def nasals( **kwargs ):
+	return consonants( phoneme_classes = [ 'nasal' ], **kwargs )
+#---------------------------------------------------------------------------------------------------------------------------------------------------#
+def laterals( **kwargs ):
+	return consonants( phoneme_classes = [ 'lateral' ], **kwargs )
+#---------------------------------------------------------------------------------------------------------------------------------------------------#
+def diphthongs( **kwargs ):
+	return vowels( phoneme_classes = [ 'diphthong' ], **kwargs )
+#---------------------------------------------------------------------------------------------------------------------------------------------------#
+def monophthongs( **kwargs ):
+	return vowels( phoneme_classes = [ 'monophthong' ], **kwargs )
+#---------------------------------------------------------------------------------------------------------------------------------------------------#
+def vowels(
+	phoneme_classes = [ 'monophthong', 'diphthong' ],
+	**kwargs,
+	):
+	phoneme_classes = check_if_list_is_valid( phoneme_classes, str )
+	vowels = phonemes.loc[
+		( phonemes[ 'phoneme_type' ] == 'vowel' ) &
+		( phonemes[ 'phoneme_class' ].isin( phoneme_classes ) )
+		].sampa.to_numpy()
+	return get_phoneme_list(
+		data = vowels,
+		**kwargs,
+		)
+#---------------------------------------------------------------------------------------------------------------------------------------------------#
+#print( vowels( phoneme_classes = 'monophthong' ) )
+#print( single_consonants() )
+
+#vowels = vowels( phoneme_class = 'monophthong' )
+#print( vowels )
