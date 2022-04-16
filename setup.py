fd4fe36822a8f55337e73a5771815a34beb0ca62
PaKr1234 <paul_konstantin.krug@tu-dresden.de>
2022-04-16 16:12:54 +0200

    without TO build incl dirs update

--- setup.py
+++ setup.py
@@ -122,12 +122,12 @@ EXT_MODULES = cythonize(
         #      include_dirs=[np.get_include()],#, './src/', './src/targetoptimizer-backend/']
         #      ),
         Extension( 'VocalTractLab.VocalTractLabApi',
-              ['./VocalTractLab/VocalTractLabApi.pyx'],
+              [ os.path.join( WORKING_PATH, 'VocalTractLab/VocalTractLabApi.pyx' ) ],
               language="c",
-              libraries=['./VocalTractLab/VocalTractLabApi'],
-              library_dirs=[ './', './VocalTractLab/'],
-              include_dirs=[np.get_include(), './VocalTractLab/'],
-              runtime_library_dirs=[ './', './VocalTractLab/', './VocalTractLab/VocalTractLabApi' ],
+              libraries=[ os.path.join( WORKING_PATH, 'VocalTractLab/VocalTractLabApi' ) ],
+              library_dirs=[ WORKING_PATH, os.path.join( WORKING_PATH, 'VocalTractLab/' ), os.path.join( WORKING_PATH, 'VocalTractLab/VocalTractLabApi' ) ],
+              include_dirs=[ np.get_include(), WORKING_PATH, os.path.join( WORKING_PATH, 'VocalTractLab/' ), os.path.join( WORKING_PATH, 'VocalTractLab/VocalTractLabApi' ) ],
+              runtime_library_dirs=[ WORKING_PATH, os.path.join( WORKING_PATH, 'VocalTractLab/' ), os.path.join( WORKING_PATH, 'VocalTractLab/VocalTractLabApi' ) ],
               ),
     ]
 )
