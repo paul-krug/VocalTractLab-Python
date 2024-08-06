09a321cff8938fe4df050e920ae543ae3fca5755
paul-krug <paul_konstantin.krug@tu-dresden.de>
2024-08-06 17:23:16 +0200

    add f0 extraction

--- vocaltractlab/audioprocessing.py
+++ vocaltractlab/audioprocessing.py
@@ -19,6 +19,8 @@ from typing import Optional
 from typing import Dict
 from numpy.typing import ArrayLike
 
+from .utils import strictly_increasing
+
 
 
 torch.set_num_threads(1)
@@ -187,3 +189,111 @@ def amplitude_to_db(
         np.abs(x) ** 2,
         **kwargs,
         )
+
+def f0(
+        x: Union[str, torch.Tensor, ArrayLike],
+        sr_in: int = None,
+        upper_f0_limit: int = 400,
+        lower_f0_limit: int = 50,
+        ):
+    
+    try:
+        import parselmouth
+    except ImportError:
+        raise ImportError(
+            """
+            You need to install the library 'parselmouth'
+            to be able to extract F0 from audio.
+            """
+            )
+
+    # Check if x is a path to a file.
+    if isinstance( x, str ):
+        x, sr_in = torchaudio.load(x)
+    elif sr_in is None:
+        raise ValueError(
+            "Must provide sr_in if x is not a path."
+            )
+
+    # If x is a tensor, convert to numpy array
+    if isinstance( x, torch.Tensor ):
+        x = x.numpy()
+
+    sr_f0 = 100
+
+    pitch_pm = parselmouth.Sound(
+        values=x,
+        sampling_frequency=sr_in,
+    ).to_pitch()
+    pitch_pm_times = pitch_pm.xs()
+    pitch_pm_values = pitch_pm.selected_array["frequency"]
+
+    pitch_pm_samples = [
+        x + int(pitch_pm_times[0] * sr_f0 )
+        for x in range(
+            0,
+            len(pitch_pm_values),
+        )
+    ]
+
+    # sanity check
+    if not strictly_increasing(pitch_pm_samples):
+        print(pitch_pm_samples)
+        print(pitch_pm_times)
+        raise ValueError(
+            "Pitch samples are not strictly increasing."
+            )
+
+    valid_range_indices = np.where(
+        (pitch_pm_values >= lower_f0_limit)
+        & (pitch_pm_values <= upper_f0_limit)
+    )[0]
+
+    xnew = [
+        x / sr_f0
+        for x in range(
+            0,
+            int(np.ceil(x.shape[-1] / sr_in * sr_f0)),
+        )
+    ]
+    if valid_range_indices.size == 0:
+        ynew = (
+            1
+            + np.abs(
+                np.random.normal(
+                    0.0,
+                    1.0,
+                    size=len(xnew),
+                )
+            )
+        ) * lower_f0_limit
+    else:
+        ynew = np.interp(
+            xnew,
+            pitch_pm_times[valid_range_indices],
+            pitch_pm_values[valid_range_indices],
+        )
+
+    voiced_flag = np.zeros(len(xnew))
+    voiced_flag[
+        [x + int(pitch_pm_times[0] * sr_f0) for x in valid_range_indices]
+    ] = 1
+
+    f0_times = pitch_pm_times[valid_range_indices]
+    f0_values = pitch_pm_values[valid_range_indices]
+
+    f0 = np.array( [
+            f0_times,
+            f0_values,
+            ]
+        ).T
+
+    # concatenate interpolated f0 and voice flag to feature of shape (timesteps, 2)
+    f0_feature = np.array(
+        [
+            ynew,
+            voiced_flag,
+        ]
+    ).T
+
+    return f0, f0_feature
--- vocaltractlab/utils.py
+++ vocaltractlab/utils.py
@@ -68,3 +68,12 @@ class Multiple:
                 self.latex,
                 )
             )
+    
+def strictly_increasing( L ):
+    return all(
+        x < y
+        for x, y in zip(
+            L,
+            L[1:],
+        )
+    )
