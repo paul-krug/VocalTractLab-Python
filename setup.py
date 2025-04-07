b6330de397110fd1a8dc67eab46730d9e804665c
paul-krug <paul_konstantin.krug@tu-dresden.de>
2025-04-07 23:44:18 +0530

    update version

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ install_requires = [
 
 setup_args = dict(
     name='VocalTractLab',
-    version='0.5.4.dev1',
+    version='0.5.5',
     description='High-performance articulatory speech synthesis in Python',
     long_description_content_type='text/markdown',
     long_description=open('README.md').read(),
